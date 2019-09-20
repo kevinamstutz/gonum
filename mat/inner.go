6496c620acbac6a1d13f9efcb3dce5c376922399
Kevin Amstutz <kevin@amstutz.io>
2019-09-20 13:57:26 -0700

    Update inner.go

--- mat/inner.go
+++ mat/inner.go
@@ -5,9 +5,9 @@
 package mat
 
 import (
-	"gonum.org/v1/gonum/blas"
-	"gonum.org/v1/gonum/blas/blas64"
-	"gonum.org/v1/gonum/internal/asm/f64"
+	"github.com/kevinamstutz/gonum/blas"
+	"github.com/kevinamstutz/gonum/blas/blas64"
+	"github.com/kevinamstutz/gonum/internal/asm/f64"
 )
 
 // Inner computes the generalized inner product
